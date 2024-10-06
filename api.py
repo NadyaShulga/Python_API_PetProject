3c6bfbc638b364c501a2283cf7497e982c5bfac7
Nadya Shulga <118784278+NadyaShulga@users.noreply.github.com>
2024-10-05 22:45:35 -0400

    update tests (#1)

--- api.py
+++ api.py
@@ -1,6 +1,5 @@
 import json
 import requests
-
 import settings
 
 
@@ -26,36 +25,38 @@ class Pets:
         headers = {'Authorization': f'Bearer {my_token}'}
         res = requests.get(self.base_url + 'users', headers=headers)
         status = res.status_code
+        amount = res.json
         my_id = res.text
-        return status, my_id
+        return status, amount, my_id
 
     def post_pet(self) -> json:
-        """Request to Swagger to post a new pet"""
+        """Request to Swagger to create a new pet"""
         my_token = Pets().get_token()[0]
         my_id = Pets().get_token()[2]
         headers = {'Authorization': f'Bearer {my_token}'}
         data = {"id": my_id,
                 "name": 'Rubik', "type": 'dog', "age": 3, "owner_id": my_id}
+
         res = requests.post(self.base_url + 'pet', data=json.dumps(data), headers=headers)
         pet_id = res.json()['id']
         status = res.status_code
-        return status, pet_id
+        return pet_id, status
 
     def get_pet_id(self) -> json:
         """Request to Swagger to get data about a pet using pet's id"""
         my_token = Pets().get_token()[0]
-        headers = {'Authorization': f'Bearer {my_token}'}
         pet_id = Pets().post_pet()[0]
+        headers = {'Authorization': f'Bearer {my_token}'}
         res = requests.get(self.base_url + f'pet/{pet_id}', headers=headers)
         pet_data = res.json()
         status = res.status_code
-        return status, pet_data
+        return pet_data, status
 
     def post_pet_photo(self):
         my_token = Pets().get_token()[0]
         pet_id = Pets().post_pet()[0]
         headers = {'Authorization': f'Bearer {my_token}'}
-        file_path = 'tests/photo/dog.jpg'
+        file_path = 'C:/Users/shulg/PycharmProjects/Python_API_PetProject/tests/photo/dog.jpg'
         file = open(file_path, 'rb')
         doc_picture_binary = {'pic': ('image.jpg', file, 'image/jpeg')}
         res = requests.post(self.base_url + f'pet/{pet_id}/image', headers=headers, files=doc_picture_binary)
@@ -63,51 +64,44 @@ class Pets:
         link = res.json()['link']
         return status, link
 
-    def patch_pet(self) -> json:
+    def edit_pet(self) -> json:
         """Request to Swagger to change a part of data about a pet"""
         my_token = Pets().get_token()[0]
-        my_id = Pets().get_token()[1]
-        pet_id = Pets().post_pet()[1]
+        my_id = Pets().get_token()[2]
+        pet_id = Pets().post_pet()[0]
         headers = {'Authorization': f'Bearer {my_token}'}
         pet_new_data = {"id": pet_id,
                         "name": 'Kira', "type": 'cat', "age": 3, "owner_id": my_id}
         res = requests.patch(self.base_url + 'pet', data=json.dumps(pet_new_data), headers=headers)
+        pet_id = res.json()['id']
         status = res.status_code
-        pet_new_data = res.json()
-        return status, pet_new_data
+        return status, pet_id
 
     def put_pet_like(self) -> json:
         """Request to Swagger to put a like to a pet"""
         my_token = Pets().get_token()[0]
-        headers = {'Authorization': f'Bearer {my_token}'}
         pet_id = Pets().post_pet()[0]
-        print(pet_id)
+        headers = {'Authorization': f'Bearer {my_token}'}
         res = requests.put(self.base_url + f'pet/{pet_id}/like', headers=headers)
-        like = res.json()
         status = res.status_code
-        print(status)
-        return status, like
+        return status, pet_id
 
     def put_pet_comment(self) -> json:
         """Request to Swagger to put a message to a pet"""
         my_token = Pets().get_token()[0]
-        my_id = Pets().get_token()[2]
-        headers = {'Authorization': f'Bearer {my_token}'}
         pet_id = Pets().post_pet()[0]
-        print(pet_id)
-        data = {"id": my_id, "message": 'My super-puper-cool dog'}
+        headers = {'Authorization': f'Bearer {my_token}'}
+        data = {"message": 'My super-puper-cool dog'}
         res = requests.put(self.base_url + f'pet/{pet_id}/comment', data=json.dumps(data), headers=headers)
         status = res.status_code
         message = res.json()
-        print(status)
         return status, message
 
-
-Pets().get_token()
-Pets().get_list_users()
-Pets().post_pet()
-Pets().get_pet_id()
-Pets().post_pet_photo()
-Pets().patch_pet()
-Pets().put_pet_like()
-Pets().put_pet_comment()
+    def delete_pet(self):
+        """Request to the Swagger to delete a pet"""
+        my_token = Pets().get_token()[0]
+        pet_id = Pets().post_pet()[0]
+        headers = {'Authorization': f'Bearer {my_token}'}
+        res = requests.delete(self.base_url + f'pet/{pet_id}', headers=headers)
+        status = res.status_code
+        return status
